8c9b04fa53b6d12bb8672fbfd1558fc644c8c6ed
Anastassiya <anastassiya.murashova1617@gmail.com>
2023-06-08 18:40:46 +0300

    Removed extra code

--- Assets/code/balaMovimiento.cs
+++ Assets/code/balaMovimiento.cs
@@ -2,26 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// public class balaMovimiento : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start() {
-//         Destroy (gameObject, 0.5f);
-        
-//     }
-
-//     // Update is called once per frame
-//     void Update() {
-//         if(movement.direccionBala == true && movement. ParardireccionBala == false) {
-//         transform.Translate (new Vector2 (Time.deltaTime * 7,0));
-//         }
-//         if(movement.direccionBala == false && movement. ParardireccionBala == true) {
-//         transform.Translate (new Vector2 (-Time.deltaTime * 7,0));
-//         }
-//     }
-// }
-
-
 public class balaMovimiento : MonoBehaviour
 {
     // Start is called before the first frame update
